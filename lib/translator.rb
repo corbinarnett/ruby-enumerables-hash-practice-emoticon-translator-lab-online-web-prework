4d2ae49aa30e30db24fd65ffb6419b1fec761ba2
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-25 21:15:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
-# require modules here
 
-def load_library
-  # code goes here
+require "yaml"
+
+def load_library(yml_file) #passing emoticons.yml as parameter. yml_file could be written as anything x, y, etc.
+emoticons = YAML.load_file(yml_file) #setting new var emoticons to equal loaded yaml file: emoticons.yml
+
+    new_hash = {
+      'get_meaning' => {},
+      'get_emoticon' => {}
+    }
+
+emoticons.each do |key,value|
+new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+new_hash['get_meaning'][value[1]] = key
+end
+
+new_hash
 end
 
 def get_japanese_emoticon
