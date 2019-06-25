9173b86f6e56ebb94159881da70de7b73a7727bc
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-25 21:24:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ emoticons = YAML.load_file(yml_file) #setting the var that i created 'emoticons'
       'get_emoticon' => {} #english emoticons "O:)"
     }
 
-    
+
 #angel: KEY
 #  - "O:)"      VALUE[0]
 #  - "☜(⌒▽⌒)☞"  VALUE[1]
